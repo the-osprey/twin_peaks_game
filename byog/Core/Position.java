323a1056e4fddc363df51eb3f24fad3dc38ef8a2
the-osprey <evnwalsh@gmail.com>
2018-07-15 09:09:29 -0700

    work on new pathfinding algorithm in router class

--- byog/Core/Position.java
+++ byog/Core/Position.java
@@ -6,10 +6,13 @@ public class Position implements Serializable {
     private static final long serialVersionUID = 8358178841294L;
     int x;
     int y;
+    Position[] adj = new Position[]{new Position(this.x + 1, this.y),
+            new Position(this.x - 1, this.y),
+            new Position(this.x, this.y + 1),
+            new Position(this.x, this.y - 1)};
 
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
 }
